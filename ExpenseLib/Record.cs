d28e723b5e69f7dcc15925d458e55e54230cc407
Orville Huang <ooorville@gmail.com>
2020-04-12 15:05:53 -0400

    update Record

--- ExpenseLib/Record.cs
+++ ExpenseLib/Record.cs
@@ -4,5 +4,10 @@ namespace ExpenseLib
 {
     public class Record
     {
+        public bool Income { get; set; }
+        public int Category { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Memo { get; set; }
     }
 }
