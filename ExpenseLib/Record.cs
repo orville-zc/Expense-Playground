d7a99b6c15303fc668dcf099b995be4f2cfff034
Orville Huang <ooorville@gmail.com>
2020-05-18 18:52:45 -0400

    introduce unit and qty

--- ExpenseLib/Record.cs
+++ ExpenseLib/Record.cs
@@ -12,25 +12,22 @@ namespace ExpenseLib
         //true when this record is an expense
         //false for income
         public string Memo { get; set; }
-
-        private void Init(decimal amt, int cat, bool exp, string memo)
-        {
-            Amount = amt;
-            Category = cat;
-            Memo = memo;
-            Exp = exp;
-        }
-        public Record(decimal amt, int cat, bool exp, string memo)
-        {
-            Init(amt, cat, exp, memo);
-            Date = DateTime.Today;
-        }
+        public double Qty { get; set; }
+        public int Unit { get; set; }
 
         [JsonConstructor] //when reading from json, use this constructor
-        public Record(decimal amt, int cat, DateTime date, bool exp, string memo)
+        public Record(decimal amount, int? category, DateTime? date, bool? exp, string memo, double? qty, int? unit)
         {
-            Init(amt, cat, exp, memo);
-            Date = date;
+            if (amount <= 0m) throw new ArgumentException("The amount should be positive.");
+            if (unit != null && qty == null)
+                throw new ArgumentException("It's meaningless to store the unit without the quantity.", "qty");
+            Amount = amount;
+            Category = (category == null) ? -1 : (int)category;
+            Memo = memo;
+            Exp = (exp == null) ? true : (bool)exp;
+            Qty = (qty > 0.0) ? (double)qty : 0.0;
+            Unit = (unit == null) ? -1 : (int)unit;
+            Date = (date == null) ? DateTime.Today : (DateTime)date;
         }
     }
 }
---
+++ ExpenseLib/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ExpenseLib
+{
+    class Unit
+    {
+        public static List<string> GetUnit()
+        {
+            return new List<string>
+            {
+                "g", "lbs", "kg"
+            };
+        }
+        //default units
+    }
+}
